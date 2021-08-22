7a506d19ad9a7e253ff52ccbc8dda88c4cca74ad
Rimuzakki <rimuzakki@gmail.com>
2021-08-23 01:12:40 +0700

    update db

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'remotemysql.com'),
+        host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'lxh3DfyaAS'),
-        username: env('DATABASE_USERNAME', 'lxh3DfyaAS'),
-        password: env('DATABASE_PASSWORD', 'ipB1dDiKxf'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
